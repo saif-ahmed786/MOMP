7ed3f4bf38af9079d6536a0c0de4d42a2cee9917
Saif <devsaif0@gmail.com>
2025-09-18 22:08:41 +0200

    Metrics fetch done

--- main.py
+++ main.py
@@ -1,7 +1,89 @@
 import psutil
+import platform
 import time
+from datetime import datetime
+def system_details():
+    uname = platform.uname()
+    print(f"System: {uname.system}")
+    print(f"Node Name: {uname.node}")
+    print(f"Release: {uname.release}")
+    print(f"Version: {uname.version}")
+    print(f"Machine: {uname.machine}")
+    print(f"Processor: {uname.processor}")
+
+def system_cpu_details():
+    # CPU Info
+    print("="*40, "CPU Info", "="*40)
+    print("Physical cores:", psutil.cpu_count(logical=False))
+    print("Total cores:", psutil.cpu_count(logical=True))
+    cpufreq = psutil.cpu_freq()
+    print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+    print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+    print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+    print("CPU Usage Per Core:")
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+        print(f"Core {i}: {percentage}%")
+    print("Total CPU Usage:", psutil.cpu_percent(), "%")
+
+def system_memory_details():
+    # Memory Information
+    print("="*40, "Memory Information", "="*40)
+    svmem = psutil.virtual_memory()
+    print(f"Total: {get_size(svmem.total)}")
+    print(f"Available: {get_size(svmem.available)}")
+    print(f"Used: {get_size(svmem.used)}")
+    print(f"Percentage: {svmem.percent}%")
+    print("\nSwap Memory:")
+    swap = psutil.swap_memory()
+    print(f"Total: {get_size(swap.total)}")
+    print(f"Free: {get_size(swap.free)}")
+    print(f"Used: {get_size(swap.used)}")
+    print(f"Percentage: {swap.percent}%")
+
+def system_disk_details():
+    # Disk Information
+    print("="*40, "Disk Information", "="*40)
+    print("Partitions and Usage:")
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        print(f"=== Device: {partition.device} ===")
+        print(f"  Mountpoint: {partition.mountpoint}")
+        print(f"  File system type: {partition.fstype}")
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+            print(f"  Total Size: {get_size(partition_usage.total)}")
+            print(f"  Used: {get_size(partition_usage.used)}")
+            print(f"  Free: {get_size(partition_usage.free)}")
+            print(f"  Percentage: {partition_usage.percent}%")
+        except PermissionError:
+            print("  Permission Denied")
+
+def system_network_details():
+    # Network Information
+    print("="*40, "Network Information", "="*40)
+    if_addrs = psutil.net_if_addrs()
+    for interface_name, interface_addresses in if_addrs.items():
+        for address in interface_addresses:
+            print(f"=== Interface: {interface_name} ===")
+            if str(address.family) == 'AddressFamily.AF_INET':
+                print(f"  IP Address: {address.address}")
+                print(f"  Netmask: {address.netmask}")
+                print(f"  Broadcast IP: {address.broadcast}")
+            elif str(address.family) == 'AddressFamily.AF_PACKET':
+                print(f"  MAC Address: {address.address}")
+                print(f"  Netmask: {address.netmask}")
+                print(f"  Broadcast MAC: {address.broadcast}")
+
+    net_io = psutil.net_io_counters()
+    print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
+    print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
 
 def monitor_systems():
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
+    print(f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}")
+    
+    
     cpu_usage = psutil.cpu_percent(interval=1)
     print(cpu_usage)
     memory = psutil.virtual_memory()
@@ -12,8 +94,23 @@ def monitor_systems():
     
     network = psutil.net_io_counters()
     print(f"Bytes Sent: {network.bytes_sent}, Bytes Received: {network.bytes_recv}")
-    
+
     time.sleep(5)
 
+
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g. 1253656 => '1.20MB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+
+
 if __name__ == '__main__':
+    system_details()
     monitor_systems()
